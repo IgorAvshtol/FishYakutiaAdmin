f8c8fe8e165b36040c9a19a0833dadcd5bad19ce
ihar <iharrwe@gmail.com>
2023-07-06 12:14:43 +0300

    fix/add vite plugin for build;

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import topLevelAwait from 'vite-plugin-top-level-await';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), topLevelAwait({
+    promiseExportName: "__tla",
+    promiseImportName: i => `__tla_${i}`
+  })],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
